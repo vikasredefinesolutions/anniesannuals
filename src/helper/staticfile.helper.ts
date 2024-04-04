5a5f3833265bf934964c328316ed1cc893f92945
Vikas R. Patel <vikas@kaushalaminc.com>
2024-04-04 22:22:31 +0530

    Fix json path

--- src/helper/staticfile.helper.ts
+++ src/helper/staticfile.helper.ts
@@ -14,8 +14,7 @@ export type tAdminConfigs = typeof adminConfigs;
 export type tHomePageMetaData = typeof homePageMetaData;
 
 export const getStaticHeaderSubMenu = async (): Promise<tHeaderSubMenuFile> => {
-  const headerSubMenuFile = await readFile(
-    process.cwd() + '/staticData/headerSubMenu.json',
+  const headerSubMenuFile = await readFile('staticData/headerSubMenu.json',
     'utf-8',
   );
 
